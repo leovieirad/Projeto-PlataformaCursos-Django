78dfa070e199d12cb2bb3ac5a799b8f7910cbb7f
leovieirad <leovieiradsz@gmail.com>
2025-06-25 18:29:18 -0300

    Configurando administração para modelos Curso e Aula

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Curso, Aula
 
-# Register your models here.
+class AulaInline(admin.TabularInline):
+    model = Aula
+    extra = 1
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'slug')
+    prepopulated_fields = {'slug': ('titulo',)}
+    inlines = [AulaInline]
+
+@admin.register(Aula)
+class AulaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'curso')
