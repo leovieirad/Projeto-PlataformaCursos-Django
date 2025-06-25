c2157c487f9e0f8e0bdf61fa821e0b5374ec5951
leovieirad <leovieiradsz@gmail.com>
2025-06-25 18:23:14 -0300

    Criando modelos Curso e Aula no app 'cursos'

--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Curso(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    imagem = models.ImageField(upload_to='cursos/imagens/', blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.titulo)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.titulo
+
+class Aula(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE, related_name='aulas')
+    titulo = models.CharField(max_length=100)
+    video_url = models.URLField(blank=True)
+    conteudo = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.titulo} ({self.curso.titulo})"
