dc56dd2eae6bcfdafb86ea3827a0cc48dea2acf6
leovieirad <leovieiradsz@gmail.com>
2025-06-25 18:15:20 -0300

    Registrando os apps 'cursos' e 'usuarios' no settings.py e executando as migrações iniciais

--- plataforma_cursos/settings.py
+++ plataforma_cursos/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps do projeto
+    'cursos',
+    'usuarios',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
