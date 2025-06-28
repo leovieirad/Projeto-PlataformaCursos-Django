76bb0f36cd7b2391ecd92c1647d4420015ef12c4
leovieirad <leovieiradsz@gmail.com>
2025-06-28 20:25:49 -0300

    Adicionando funcionalidade para marcar aulas como assistidas

--- cursos/urls.py
+++ cursos/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.lista_cursos, name='lista_cursos'),
     path('<slug:slug>/', views.detalhe_curso, name='detalhe_curso'),
     path('<slug:slug>/matricular/', views.matricular_curso, name='matricular_curso'),
-
+    path('aula/<int:aula_id>/assistir/', views.marcar_aula_assistida, name='marcar_aula_assistida'),
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from .models import Curso
-from django.shortcuts import get_object_or_404
+from .models import Curso, Aula, AulaAssistida
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+
 
 
 def lista_cursos(request):
@@ -20,3 +20,12 @@ def matricular_curso(request, slug):
     curso = get_object_or_404(Curso, slug=slug)
     Matricula.objects.get_or_create(usuario=request.user, curso=curso)
     return HttpResponseRedirect(reverse('detalhe_curso', args=[slug]))
+
+
+@login_required
+def marcar_aula_assistida(request, aula_id):
+    aula = get_object_or_404(Aula, id=aula_id)
+
+    AulaAssistida.objects.get_or_create(usuario=request.user, aula=aula)
+
+    return redirect('detalhe_curso', slug=aula.curso.slug)
