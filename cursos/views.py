ebabd5da34722e2eb8ba6b26308f509594684ca4
leovieirad <leovieiradsz@gmail.com>
2025-07-02 19:26:07 -0300

    Corrigindo import do csrf_protect para habilitar marcação de aulas com persistência

--- cursos/views.py
+++ cursos/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from .forms import ComentarioForm
+from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 import json
