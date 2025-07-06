843235c7b47ea55a031c23ec33568f7e73f6dd80
leovieirad <leovieiradsz@gmail.com>
2025-07-06 19:41:19 -0300

    Iniciando a criação da funcionalidade de perfil do usuário

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Perfil(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    foto = models.ImageField(upload_to='fotos_perfil/', blank=True, null=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Perfil de {self.user.username}"
